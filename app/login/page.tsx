007a5306966bf093d9c21665ae6de5c0476a87ca
ShrutiNavik <122103450+ShrutiNavik@users.noreply.github.com>
2026-04-20 12:25:44 +0530

    fixed

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,4 @@
 'use client'
-import Navbar from '@/components/Navbar'
-import Link from 'next/link'
 import { useState } from 'react'
 import { signIn } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
